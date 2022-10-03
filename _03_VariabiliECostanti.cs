07ebd068a19f19887cd0034b36f13130a2c7b1cd
salvo720 <salvocelano91@gmail.com>
2022-10-03 20:07:47 +0200

    stopped at 00:00 minutes , Tipi di dati , from link ( https://www.youtube.com/watch?v=fgqgCp5l5YM&list=PLP5MAKLy8lP-3Z1uXqApUi5aByTWAla6t&index=4 )

---
+++ _03_VariabiliECostanti.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class _03_VariabiliECostanti
+{
+    public _03_VariabiliECostanti()
+	{
+        static void Main(string[] args)
+        {
+            //inizializzazione
+            string nome1 = "Luca";
+            
+            //dichiarazione
+            string cognome1;
+            
+            //assegnazione
+            cognome1 = "Rossi";
+
+            //riassegnazione 
+            const string nome = "Marco";
+            string cognome = "Verdi";
+            int eta = 18;
+
+
+
+
+
+            Console.WriteLine("Ciao io sono" + nome);
+            Console.WriteLine("Ho " + eta + " anni");
+            Console.WriteLine( nome + " e il mio primo nome ");
+           
+            Console.WriteLine("sono nato il 25 novembre ");
+            // Console.Read(); ci consente di mantenere  il programma in esecuzione perche aspetta un input dell'utente 
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ studyCSharp/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace studyCSharp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Console.Read(); ci consente di mantenere  il programma in esecuzione perche aspetta un input dell'utente 
+            Console.Read();
+        }
+    }
+}
+
+
+// le funzione locali non possono includere modificatori di classe come public o protecded
+
+// i namespace sono come i package di java , quindi i namespace raggruno del codice andando a dare una struttura al nostro progetto
+
+// c# e un linguaggio orientato agli oggetti 
+
+// Main e il nostro entry point o metodo che viene eseguito quando inizia il programma 
+
+// visual studio usa il colore verde per indicare che stiamo usando qualcosa di una classe in guarda l'esempio  Console.WriteLine("Hello World!");
+
+// c# e un linguaggio fortemente tipizzato , quindi bisogna sempre specificare il tipo della variabile 
+
+// su c# i ; sono obbligatori 
