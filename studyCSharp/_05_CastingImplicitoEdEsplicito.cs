97e2f9e6865796fd30f88ffa9b65c94e9a5d50ed
salvo720 <salvocelano91@gmail.com>
2022-10-05 14:00:04 +0200

    stopped at 00:00 minutes , Lavorare con i numeri , from link ( https://www.youtube.com/watch?v=e8zEZt3oX3E&list=PLP5MAKLy8lP-3Z1uXqApUi5aByTWAla6t&index=7 )

---
+++ studyCSharp/_05_CastingImplicitoEdEsplicito.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.VisualBasic;
+using System;
+
+namespace _05_CastingImplicitoEdEsplicito // Note: actual namespace depends on the project name.
+{
+    internal class _05_CastingImplicitoEdEsplicito
+    {
+        static void Main(string[] args)
+        {
+            //Casting :
+            //1) impicito : c# consente di passare va un tipo piu piccolo ad uno piu grande , ad esempio da int a long , da int a float ,
+            // ma non consente ad un tipo piu grande di diventare piu piccolo 
+            int numero1 = 10;
+            int lungo = numero1;
+
+            //2) esplicito : viene usato per passare da un tipo piu grande ad uno piu piccolo , ad esempio da float a int 
+            float fluttuante = 10.2598F;
+            int floatInt = (int)fluttuante;
+
+            //3) metodi di conversione : quando i tipi di dati non sono uguali o non sono della stessa categoria usiamo l'oggetto convert 
+
+            int numero2 = 10;
+            string stringa;
+            //stringa = (string)numero2; // nota che andra in errore 
+            stringa = Convert.ToString(numero2);
+
+
+        }
+    }
+}
+
