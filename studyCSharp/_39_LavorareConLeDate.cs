8cdae3b0a520eb2605e266803363fc4af4afd992
salvo720 <salvocelano91@gmail.com>
2022-10-12 19:50:38 +0200

    stopped at 6:30 minutes (  alla formatazzione delle date ) , Lavorare con le date (DateTime) , from link ( https://www.youtube.com/watch?v=NUaR22uBtBQ&list=PLP5MAKLy8lP-3Z1uXqApUi5aByTWAla6t&index=39 )

--- studyCSharp/_39_LavorareConLeDate.cs
+++ studyCSharp/_39_LavorareConLeDate.cs
@@ -15,7 +15,37 @@ namespace studyCSharp // Note: actual namespace depends on the project name.
     {
         public static void function_39_LavorareConLeDate()
         {
-        
+
+            //Lavorare con le date (datetime)
+            //creare una data , oggi , adesso , propieta , kind 
+            // aggiungere , sottrarre , formattazione
+            // 
+
+            DateTime data = new DateTime();
+            Console.WriteLine("data ( DateTime() ) : " +  data); // 01/01/0001 00:00:00 punto da cui iniza a contare per le date 
+
+            DateTime data1 = new DateTime(2021, 10, 10, 12, 39, 45);
+            Console.WriteLine("data1 ( DateTime(2021, 10, 10, 12, 39, 45); )  : " + data1);
+
+            Console.WriteLine("data1.Year ( DateTime(2021, 10, 10, 12, 39, 45); )  : " + data1.Year);
+            Console.WriteLine("data1.Day ( DateTime(2021, 10, 10, 12, 39, 45); )  : " + data1.Day);
+            Console.WriteLine("data1.DayOfWeek ( DateTime(2021, 10, 10, 12, 39, 45); )  : " + data1.DayOfWeek);
+            Console.WriteLine("data1.DayOfYear ( DateTime(2021, 10, 10, 12, 39, 45); )  : " + data1.DayOfYear); // giorni alla fine dell'anno 
+            Console.WriteLine("data1.Minute ( DateTime(2021, 10, 10, 12, 39, 45); )  : " + data1.Minute); 
+            Console.WriteLine("data1.AddDays(11) ( DateTime(2021, 10, 10, 12, 39, 45); )  : " + data1.AddDays(11)); // aggiunge 11 giorni alla data attuale 
+            //possiamo  andare a combinare le varie propeta 
+            Console.WriteLine("data1.AddYears(2) ( DateTime(2021, 10, 10, 12, 39, 45); )  : " + data1.AddYears(2).DayOfWeek); // aggiunge 2 anni alla data attuale e prende il giorno della settimana 
+
+            DateTime data2 = new DateTime(2021, 2, 10);
+            Console.WriteLine("data1.Subtract(data2) ( DateTime(2021, 10, 10, 12, 39, 45); )  : " + data1.Subtract(data2)); // aggiunge 2 anni alla data attuale e prende il giorno della settimana 
+
+
+            // DateTime:
+            Console.WriteLine("DateTime.UtcNow : " + DateTime.UtcNow);  //.UtcNow: si basa su greenwich , quindi l'ora sara 1 ora indietro rispetto all'italia 
+            Console.WriteLine("DateTime.Now : " + DateTime.Now);  //.Now: prende data e ora di adesso
+            Console.WriteLine("DateTime.Today : " + DateTime.Today); //  .Today : prende la data di oggi 
+            Console.WriteLine();
+
         }
     }
 
